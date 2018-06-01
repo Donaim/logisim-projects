e89a4198b20f34cefa2f5a0398cff04cd2a1325b
Donaim <3Donaim@gmail.com>
2018-06-01 14:14:11 +0200

    factorial works!

--- cpu24/ass.py
+++ cpu24/ass.py
@@ -3,7 +3,6 @@ import sys
 import os
 from instructions import *
 
-
 def bin_to_hex(bin: str) -> str:
     return hex(int(bin, base=2))[2:]
 
@@ -12,7 +11,7 @@ def is_code_line(line: str) -> bool:
 
 def pad_args(args: list):
     for i in range(len(args), 3):
-        args.append('0')
+        args.append('%')
 
 def isnumber(string: str):
     try:
@@ -20,20 +19,37 @@ def isnumber(string: str):
         return True
     except:
         return False
+def ischar(string: str):
+    l = len(string)
+    if l < 3: 
+        return False
+    if string[0] != "\'" or string[-1] != '\'':
+        return False
+    if l > 4: 
+        return False
+    if l == 4 and string[1] != '\\':
+        return False
+    return True
 
 def split_line(line: str):
     (line, _, _) = line.partition('#')
     (out, _, com) = line.partition('=')
     out = out.strip()
+    com = com.strip()
 
     if len(com) == 0:
         raise Exception('command in line "{}" cannot be empty!'.format(line))
 
     csp = com.split()
+    csp = list(map (str.strip, csp))
+
     op = csp[0]
     if not op in operators: # if not operator, then maybe input
         if isnumber(com):
-            return ((out, com), True)
+            return ((out, com), "number")
+        elif ischar(com):
+            com = convert_char_to_decimal(com)
+            return ((out, com), "number")
         else:
             csp = ['x', op]
             op = csp[0]
@@ -47,7 +63,7 @@ def split_line(line: str):
 
     pad_args(args)
 
-    return ((out, op, args[0], args[1], args[2]), False)
+    return ((out, op, args[0], args[1], args[2]), "expr")
 
 def translate_part(p: str, di) -> str:
     try: return di[p]
@@ -60,6 +76,13 @@ def convert_decimal_to_binary(dec: str) -> str:
     zeros = "0" * (20 - len(s))
     s = zeros + s
     return s
+def convert_char_to_decimal(ch: str) -> str:
+    ch = ch.strip("\'")
+    if ch == "\\n":
+        ch = '\n'
+    elif ch == "\\t":
+        ch = '\t'
+    return str(ord(ch))
 
 def get_literal_command_binary(vs: list) -> str:
     (outp, num) = vs
@@ -68,32 +91,36 @@ def get_literal_command_binary(vs: list) -> str:
     numb = convert_decimal_to_binary(num)
 
     binary = outp + numb
-    print ("binary = {}".format(binary))
+    # print ("binary = {}".format(binary))
     return binary
 
 def get_normal_command_binary(vs: list) -> str:
     (outp, op, x, y, z) = vs
 
-    print ("splitted={}".format([outp, op, x, y, z]))
+    # print ("splitted={}".format([outp, op, x, y, z]))
 
     outp = translate_part(outp, output_adresses)
     op   = translate_part(op, operators)[0]
     x    = translate_part(x, input_adresses)
     y    = translate_part(y, input_adresses)
     z    = translate_part(z, input_adresses)
-    print ("binary splitted={}".format([x, y, z, outp, op]))
+    # print ("binary splitted={}".format([x, y, z, outp, op]))
 
     binary = outp + '100' + op + x + y + z
-    print ("binary = {}".format(binary))
+    # print ("binary = {}".format(binary))
     return binary
-
+def get_binary_from_line(vs: list, rtype) -> str:
+    if rtype == 'expr':
+        return get_normal_command_binary(vs)
+    if rtype == 'number':
+        return get_literal_command_binary(vs)
+    raise NotImplementedError('unknown rtype: {}'.format(rtype))
 def translate_line(line: str) -> str:
     if not is_code_line(line): return None
 
-    (vs, is_liter) = split_line(line)
-    binary = get_literal_command_binary(vs) if is_liter else get_normal_command_binary(vs)
-    he = bin_to_hex(binary)
-    return he
+    (vs, rtype) = split_line(line)
+    binary = get_binary_from_line(vs, rtype)
+    return bin_to_hex(binary)
 
 def assemble_file(path: str) -> list:
     if not os.path.isfile(path):
@@ -133,6 +160,7 @@ if __name__ == "__main__":
     print("")
     
     lines = assemble_file(sys.argv[1])
-    write_lines("code2.tsv", lines)
+    output =  sys.argv[2] if len(sys.argv) > 2 else 'a.out' 
+    write_lines(output, lines)
 
     print ("\nEND")
